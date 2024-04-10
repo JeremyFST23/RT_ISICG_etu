b2936a8031cfcd7bb8528c39d2c88b762fcedeba
vacher27 <vacher27@FST-O-I-213-04>
2024-04-10 17:08:38 +0200

    DebutTp7

--- src/objects/implicit_surface.cpp
+++ src/objects/implicit_surface.cpp
@@ -7,7 +7,21 @@ namespace RT_ISICG
 									 const float p_tMax,
 									 HitRecord & p_hitRecord ) const
 	{
-		/// TODO
+		/*Initialize t = 0 and d = 0
+While t < D
+Let d = f (r(t))
+If d <  then return t --- intersection
+Increment t = t + d
+return ; --- no intersection*/
+		float t = p_tMin;
+		float d = 0;
+		while (t < p_tMax) {
+			p_hitRecord._point = p_ray.pointAtT(t);
+			p_hitRecord.faceNormal( p_ray.getDirection() );
+			p_hitRecord._distance = t;
+			p_hitRecord._object	  = this;
+
+		}
 		return false;
 	}
 
